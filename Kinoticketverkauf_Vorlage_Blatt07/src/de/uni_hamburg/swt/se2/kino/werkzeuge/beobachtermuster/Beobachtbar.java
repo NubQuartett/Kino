39ba3921ab0c9ec100f326e0ecfd9bceb4943e74
wuchjazz <l.jimenez.hildenbrand@gmail.com>
2017-06-12 14:59:27 +0200

    Update Beobachtbar.java

--- Kinoticketverkauf_Vorlage_Blatt07/src/de/uni_hamburg/swt/se2/kino/werkzeuge/beobachtermuster/Beobachtbar.java
+++ Kinoticketverkauf_Vorlage_Blatt07/src/de/uni_hamburg/swt/se2/kino/werkzeuge/beobachtermuster/Beobachtbar.java
@@ -1,26 +1,34 @@
-/**
- * 
- */
 package de.uni_hamburg.swt.se2.kino.werkzeuge.beobachtermuster;
 import java.util.HashSet;
 import java.util.Set;
 /**
- * @author Lou
- *
+ * Die abstrakte Klasse beobachtbar. Alle implementierenden Klassen
+ * können von einem Beobachter beobachtet werden und diesen
+ * über Änderungen informieren.
  */
 public abstract class Beobachtbar {
 
-	private Set<Beobachter> _beobachter;
-	
-	public Beobachtbar()
-	{
-		_beobachter = new HashSet<Beobachter>();
-	}
-	
-	/**
-	 * informiert über die aenderung, aenderung gibt den typ der aenderung an
-	 * @param aenderung
-	 */
+    /**
+     * Das Set von Beobachtern. In diesem konkreten System gibt es
+     * nur einen einzigen Beobachter: das Kassenwerkzeug.
+     */
+    private Set<Beobachter> _beobachter;
+   
+    /**
+     * Der Konstruktor der Klasse Beobachtbar. Es wird ein neues HashSet
+     * für die Beobachter erzeugt.
+     */
+    public Beobachtbar()
+    {
+        _beobachter = new HashSet<Beobachter>();
+    }
+   
+    /**
+     * Mit dieser Methode informieren die beobachteten Klassen
+     * ihren Beobachter über eine Änderung an einem
+     * konkreten Objekt.
+     * @param aenderung das Objekt, an dem sich etwas geändert hat
+     */
     public void informiereUeberAenderung(Object aenderung)
     {
         for(Beobachter b: _beobachter)
@@ -28,11 +36,16 @@ public abstract class Beobachtbar {
             b.beachteAenderung(aenderung);
         }
     }
-    
+   
+    /**
+     * Ein Beobachter wird als Parameter übergeben und dieser wird in das
+     * HashSet von Beobachtern der beobachtbaren Klasse hinzugefügt.
+     *
+     * @param b der Beobachter
+     */
     public void setzeBeobachter(Beobachter b)
     {
         assert b != null : "Vorbedingung verletzt: beobachter != null";
         _beobachter.add(b);
     }
 }
-
