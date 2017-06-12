8fc51c02300dc40196493c73958af17786c162c9
wuchjazz <l.jimenez.hildenbrand@gmail.com>
2017-06-12 14:59:53 +0200

    Update Beobachter.java

--- Kinoticketverkauf_Vorlage_Blatt07/src/de/uni_hamburg/swt/se2/kino/werkzeuge/beobachtermuster/Beobachter.java
+++ Kinoticketverkauf_Vorlage_Blatt07/src/de/uni_hamburg/swt/se2/kino/werkzeuge/beobachtermuster/Beobachter.java
@@ -1,7 +1,17 @@
 package de.uni_hamburg.swt.se2.kino.werkzeuge.beobachtermuster;
 
+/**
+ * Das Interface Beobachter. Alle implementierenden Klassen
+ * können als Beobachter von beobachtbaren Klasse gesetzt werden.
+ */
 public interface Beobachter {
 
-   
+   /**
+    * Dem Beobachter wurde eine Änderung an einem Objekt mitgeteilt, von einer
+    * Klasse, die er beobachtet.
+    * Der Beobachter handelt entsprechend dieser Änderung.
+    *
+    * @param aenderung das veränderte Objekt
+    */
     public void beachteAenderung(Object aenderung);
 }
